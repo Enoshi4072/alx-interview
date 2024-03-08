36dea35eb18f9bf35b8d90984789e61fb0a399f5
Enoshi4072 <enockkibet6642@gmail.com>
2024-03-08 06:12:47 +0000

    Fixed pycodestyle validation

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ Working to forma a pascals triangle """
+
+
 def pascal_triangle(n):
     """
     Generate Pascal's triangle up to the nth row.
@@ -12,26 +14,16 @@ def pascal_triangle(n):
 
     If n is less than or equal to 0, an empty list is returned.
     """
-    
     if type(n) is not int or n <= 0:
         return []
 
     triangle = [[1]]
-
-    
     for i in range(1, n):
-        
         prev_row = triangle[-1]
-        
         new_row = [1]
-        
         for j in range(1, len(prev_row)):
 
             new_row.append(prev_row[j - 1] + prev_row[j])
-        
         new_row.append(1)
-        
         triangle.append(new_row)
-
-    
     return triangle
