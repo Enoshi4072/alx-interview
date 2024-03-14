50fa5ac55de7238e427b7f21f9b7e7e31c9a28df
Enoshi4072 <enockkibet6642@gmail.com>
2024-03-14 06:35:26 +0000

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,28 +1,24 @@
-#!/usr/bin/python3
-""" Coding lock boxes """
-from collections import deque
-
-
+#!/usr/bin/env python3
+""" Implementing a python function for lock boxes """
 def canUnlockAll(boxes):
-    """
-    Determine if all the boxes can be opened.
+    """Determine if all the boxes can be opened.
 
     Args:
-    - boxes (list of list of int): A list of boxes
-    where each box is represented by a list of integers.
+        boxes (List[List[int]]): A list of boxes
+        where each box is represented by a list of integers.
 
     Returns:
-    - bool: True if all boxes can be opened, False otherwise.
+        bool: True if all boxes can be opened, False otherwise.
     """
     n = len(boxes)
     visited = [False] * n
     visited[0] = True
 
-    queue = deque([0])
+    queue = [0]
 
     while queue:
-        box = queue.popleft()
-        for key in boxes[box]:
+        box = queue.pop(0)
+        for key in boxies[box]:
             if not visited[key]:
                 visited[key] = True
                 queue.append(key)
