9cdf177a2b2c87acbad55576dc2aeb06f2cb8e7b
Enoshi4072 <enockkibet6642@gmail.com>
2024-05-16 18:10:04 +0000

    Prime game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+
+def is_prime(n):
+    """Helper function to check if a number is prime."""
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+
+def isWinner(x, nums):
+    """Determines the winner of each game round."""
+    wins = {'Maria': 0, 'Ben': 0}
+    for n in nums:
+        prime_count = sum(1 for i in range(1, n+1) if is_prime(i))
+        # If prime_count is even, Ben wins. Otherwise, Maria wins.
+        if prime_count % 2 == 0:
+            wins['Ben'] += 1
+        else:
+            wins['Maria'] += 1
+
+    if wins['Maria'] > wins['Ben']:
+        return 'Maria'
+    elif wins['Maria'] < wins['Ben']:
+        return 'Ben'
+    else:
+        return None
+
+
+if __name__ == "__main__":
+    print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
